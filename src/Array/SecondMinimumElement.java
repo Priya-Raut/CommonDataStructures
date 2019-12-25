4d201a901a679321d3b3758cac4ef1b7591b261f
Priya Raut <rautpriya215@gmail.com>
2019-12-25 13:29:48 +0530

    Update SecondMinimumElement.java
    - Make code modular by writing code in methods
    - Add java docs

--- src/Array/SecondMinimumElement.java
+++ src/Array/SecondMinimumElement.java
@@ -9,20 +9,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
+ * Find second minimum element of an array
  *
  * @author piya
  */
 public class SecondMinimumElement {
-    public static void main(String args[]){
+
+    /**
+     * Accept an integer array input from user
+     *
+     * @return anArray input from user
+     */
+    public static int[] getIntegerArray(){
         int[] anArray = new int[5];
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter array elements: ");
+        System.out.println("Enter integer array elements: ");
         for (int i=0; i< anArray.length ; i++){
             anArray[i] = sc.nextInt();
         }
-        System.out.println(Arrays.toString(anArray));
+        return anArray;
+    }
+
+    /**
+     * Find second minimum element of an integer array
+     *
+     * @param anArray an integer array
+     * @return second minimum element of an array
+     */
+    public static int getSecondMinimumElement(int[] anArray){
+        System.out.println("Before sorting: " + Arrays.toString(anArray));
         Arrays.sort(anArray);
-        System.out.println(Arrays.toString(anArray));
-        System.out.println(anArray[1]);
+        System.out.println("After sorting: " + Arrays.toString(anArray));
+        return anArray[1];
+    }
+
+    public static void main(String args[]){
+        System.out.println("Second minimum element of array: " +getSecondMinimumElement(getIntegerArray()));
     }
 }
