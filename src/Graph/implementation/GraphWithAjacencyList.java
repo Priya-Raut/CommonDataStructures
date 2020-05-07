1eb9a700151d77d138729a6cc5b4696b64840152
Priya Raut <rautpriya215@gmail.com>
2020-05-07 17:52:19 +0530

    [Graph] Add public methods to get number of nodes and adjacency list

--- src/Graph/implementation/GraphWithAjacencyList.java
+++ src/Graph/implementation/GraphWithAjacencyList.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Graph.implementation;
 
 import java.util.ArrayList;
@@ -10,12 +5,12 @@ import java.util.ArrayList;
 /**
  * A class which represents graph using adjacency list
  *
- * @author piya
+ * @author Priya Raut
  */
 public class GraphWithAjacencyList {
 
-    int numberOfNodes;
-    ArrayList<ArrayList<Integer>> adjacencyList;
+    private static int numberOfNodes;
+    private static ArrayList<ArrayList<Integer>> adjacencyList;
 
     /**
      * Create adjacencyList for the graph
@@ -23,9 +18,8 @@ public class GraphWithAjacencyList {
      * @param numberOfNodes
      */
     public GraphWithAjacencyList(int numberOfNodes) {
-        this.numberOfNodes = numberOfNodes;
         //create outer list  
-        this.adjacencyList = new ArrayList<>(numberOfNodes);
+        adjacencyList = new ArrayList<>(numberOfNodes);
         //populate inner list for each item in outer list
         for (int i = 0; i < numberOfNodes; i++) {
             adjacencyList.add(new ArrayList<>());
@@ -39,9 +33,35 @@ public class GraphWithAjacencyList {
      * @param v1 a node in the graph
      * @param v2 another node in the graph
      */
-    public void addEdge(ArrayList<ArrayList<Integer>> adjacencyList, int v1, int v2) {
+    public static void addEdge(ArrayList<ArrayList<Integer>> adjacencyList, int v1, int v2) {
         adjacencyList.get(v1).add(v2); //add edge between v1 and v2
         adjacencyList.get(v2).add(v1); //also add edge between v2 and v1 since this is undirectional graph
+    }
+
+    /**
+     * Get total number of nodes from the graph
+     *
+     * @return number of nodes in the graph
+     */
+    public static int getNumberOfNodes() {
+        return 5;
+    }
+
+    /**
+     * Get adjacencyList
+     *
+     * @return AdjacencyList
+     */
+    public static ArrayList<ArrayList<Integer>> getAdjacencyList() {
+        GraphWithAjacencyList graph = new GraphWithAjacencyList(5);
+        addEdge(adjacencyList, 0, 1);
+        addEdge(adjacencyList, 0, 4);
+        addEdge(adjacencyList, 1, 2);
+        addEdge(adjacencyList, 1, 4);
+        addEdge(adjacencyList, 1, 3);
+        addEdge(adjacencyList, 2, 3);
+        addEdge(adjacencyList, 3, 4);
+        return adjacencyList;
     }
 
     /**
@@ -49,7 +69,7 @@ public class GraphWithAjacencyList {
      *
      * @param adjacencyList
      */
-    public void printEdge(ArrayList<ArrayList<Integer>> adjacencyList) {
+    public static void printEdge(ArrayList<ArrayList<Integer>> adjacencyList) {
         for (int i = 0; i < adjacencyList.size(); i++) {//for outer list
             System.out.print("head");
             for (int j = 0; j < adjacencyList.get(i).size(); j++) {//for each element in the inner list
@@ -60,14 +80,6 @@ public class GraphWithAjacencyList {
     }
 
     public static void main(String[] args) {
-        GraphWithAjacencyList graph = new GraphWithAjacencyList(5);
-        graph.addEdge(graph.adjacencyList, 0, 1);
-        graph.addEdge(graph.adjacencyList, 0, 4);
-        graph.addEdge(graph.adjacencyList, 1, 2);
-        graph.addEdge(graph.adjacencyList, 1, 4);
-        graph.addEdge(graph.adjacencyList, 1, 3);
-        graph.addEdge(graph.adjacencyList, 2, 3);
-        graph.addEdge(graph.adjacencyList, 3, 4);
-        graph.printEdge(graph.adjacencyList);
+        printEdge(getAdjacencyList());
     }
 }
