0f35e67607d6edf672f16126337ce7beda82b958
Priya Raut <rautpriya215@gmail.com>
2020-05-07 16:06:30 +0530

    fix indentation

--- src/Graph/implementation/GraphWithAjacencyList.java
+++ src/Graph/implementation/GraphWithAjacencyList.java
@@ -3,72 +3,62 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Graph;
+package Graph.implementation;
 
 import java.util.ArrayList;
 
 /**
  * A class which represents graph using adjacency list
- * 
+ *
  * @author piya
  */
 public class GraphWithAjacencyList {
-    /*
-     Data structures we are gonna need:
-     1. Array to store visited nodes
-     2. Queue to keep track of order of neighbouring nodes as we visit them
-     3. Adjacency list to store adjacent nodes a nodes
-    
-     Operations:
-     1. Add edge
-     2. Print edge
-    */
-    
+
     int numberOfNodes;
     ArrayList<ArrayList<Integer>> adjacencyList;
-    
+
     /**
      * Create adjacencyList for the graph
-     * 
-     * @param numberOfNodes 
+     *
+     * @param numberOfNodes
      */
     public GraphWithAjacencyList(int numberOfNodes) {
-      this.numberOfNodes = numberOfNodes;
-      //create outer list  
-      this.adjacencyList = new ArrayList<>(numberOfNodes);
-      //populate inner list for each item in outer list
-      for (int i=0; i<numberOfNodes; i++) {
-         adjacencyList.add(new ArrayList<>());
-      } 
+        this.numberOfNodes = numberOfNodes;
+        //create outer list  
+        this.adjacencyList = new ArrayList<>(numberOfNodes);
+        //populate inner list for each item in outer list
+        for (int i = 0; i < numberOfNodes; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
     }
-    
+
     /**
      * Add edge between two nodes in the graph (using adjacency list)
-     * 
-     * @param adjacencyList 
+     *
+     * @param adjacencyList
      * @param v1 a node in the graph
      * @param v2 another node in the graph
      */
-    public void addEdge(ArrayList<ArrayList<Integer>> adjacencyList, int v1, int v2){
+    public void addEdge(ArrayList<ArrayList<Integer>> adjacencyList, int v1, int v2) {
         adjacencyList.get(v1).add(v2); //add edge between v1 and v2
         adjacencyList.get(v2).add(v1); //also add edge between v2 and v1 since this is undirectional graph
     }
-    
+
     /**
      * Print the edges between the adjacent nodes
-     * 
-     * @param adjacencyList 
+     *
+     * @param adjacencyList
      */
-    public void printEdge(ArrayList<ArrayList<Integer>> adjacencyList){
-        for(int i=0; i<adjacencyList.size(); i++){//for outer list
+    public void printEdge(ArrayList<ArrayList<Integer>> adjacencyList) {
+        for (int i = 0; i < adjacencyList.size(); i++) {//for outer list
             System.out.print("head");
-            for(int j=0; j<adjacencyList.get(i).size(); j++){//for each element in the inner list
+            for (int j = 0; j < adjacencyList.get(i).size(); j++) {//for each element in the inner list
                 System.out.print(" --> " + adjacencyList.get(i).get(j));
             }
             System.out.println();
         }
     }
-    
+
     public static void main(String[] args) {
         GraphWithAjacencyList graph = new GraphWithAjacencyList(5);
         graph.addEdge(graph.adjacencyList, 0, 1);
